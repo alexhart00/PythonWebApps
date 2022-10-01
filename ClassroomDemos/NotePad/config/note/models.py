75b777c73e000cfcf2acf525ddd00dc0899f2482
alexhart00 <alexhart3000@gmail.com>
2022-09-30 19:38:39 -0600

    Figuring things out

--- ClassroomDemos/NotePad/config/note/models.py
+++ ClassroomDemos/NotePad/config/note/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 
-
+# Create your models here.
 class Note(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     body = models.TextField()
-
-    def __str__(self):
-        return f'{self.pk}. {self.title} - {self.author}'
